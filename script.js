7e675521ddc9d983c8db9326d765d14e8161f3a8
nanopi8788 <141821293+nanopi8788@users.noreply.github.com>
2025-12-19 00:33:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Firebase設定（ここに貼る）
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD3I5n7DTJgLG8dmuBwahc_TdwPb8FzcMk",
   authDomain: "saezuri-218c7.firebaseapp.com",
   projectId: "saezuri-218c7",
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
